17947f04f23af8e06a975976af06be61c74dec05
Szymonz <szymz99@gmail.com>
2017-05-16 20:50:23 +0200

    fourth commit

--- src/asrobots/MyRobot.java
+++ src/asrobots/MyRobot.java
@@ -11,16 +11,24 @@ public class MyRobot extends Robot {
 
 
     @Override
+    public void onScannedRobot(ScannedRobotEvent event) {
+        fire(3);
+        super.onScannedRobot(event);
+    }
+
+    @Override
     public void run() {
 
         turnGunRight(90);
 
         while (true) {
-            ahead(20);
+            ahead(30);
             turnRadarLeft(360);
             turnRadarRight(360);
-            turnLeft(10);
+            turnLeft(30);
         }
+
+
 
 
     }
